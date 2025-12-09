bc695e8332dc98aeb5ebd59e994d2dda77e0e88b
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-12-09 20:25:27 -0300

    init project

---
+++ target/debug/deps/poc_chunk_api-80bbb85d55631221.d
@@ -0,0 +1,5 @@
+/Users/luizgabriel/Desktop/poc-chunk-api/target/debug/deps/poc_chunk_api-80bbb85d55631221.d: src/main.rs
+
+/Users/luizgabriel/Desktop/poc-chunk-api/target/debug/deps/poc_chunk_api-80bbb85d55631221: src/main.rs
+
+src/main.rs:
---
+++ target/debug/poc-chunk-api.d
@@ -0,0 +1,1 @@
+/Users/luizgabriel/Desktop/poc-chunk-api/target/debug/poc-chunk-api: /Users/luizgabriel/Desktop/poc-chunk-api/src/main.rs
